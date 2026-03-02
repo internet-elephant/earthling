fce2b6605556d50568017f4ed3a1a5f7ba3ce85c
D <definternetelephant@gmail.com>
2026-03-02 18:20:01 +0000

    Initial Commit

---
+++ Earthling/EarthlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthlingApp.swift
+//  Earthling
+//
+//  Created by max on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct EarthlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
